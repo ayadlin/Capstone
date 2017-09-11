5322623e5ca31d430299678f79a4adb2e580c9ed
ale <alewolf@gmail.com>
2017-09-11 13:46:32 -0700

    almost done with sparse matrix pipeline

--- sparse_matrix_functions.py
+++ sparse_matrix_functions.py
@@ -18,3 +18,32 @@ def column_indexing(columns):
     for idx,name in enumerate(columns):
         column_index[name]=idx
     return column_index
+
+def drop_columns(X,index_to_drop):
+    to_keep = list(set(range(X.shape[1]-1))-set(index_to_drop))
+    new_X = X[:,to_keep]
+    return new_X
+
+def make_network_matrix(X, vocabulary):
+    #drug_columns_name =[i for i in vocabulary if i in drug_values]
+    drug_columns_index =[vocabulary.index(i) for i in vocabulary if i in drug_values]
+    gene_columns_index =[vocabulary.index(i) for i in vocabulary if i in gene_values]
+    #gene_columns_name =[i for i in vocabulary if i in gene_values]
+    gene_mat=X[:,gene_columns_index]
+    drug_mat=X[:,drug_columns_index]
+    network_matrix = gene_mat.T*drug_mat
+    return network_matrix
+
+def get_network_rows(vocabulary):
+    genes = {}
+    genes_columns_names=[i for i in vocabulary if i in gene_values]
+    for idx,name in enumerate(genes_columns_names):
+        genes[idx]=name
+    return genes
+
+def get_network_columns(vocabulary):
+    drugs = {}
+    drugs_columns_names=[i for i in vocabulary if i in drug_values]
+    for idx,name in enumerate(drugs_columns_names):
+        drugs[idx]=name
+    return drugs
