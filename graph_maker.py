7db80ecc1a2edcccaf4a05bdf6e576fb285dd9a2
ale <alewolf@gmail.com>
2017-09-16 19:51:37 -0700

    network

--- graph_maker.py
+++ graph_maker.py
@@ -22,7 +22,7 @@ from matplotlib import pyplot as plt
 import networkx as nx
 # %matplotlib inline
 
-vocabulary = data_frame_creator.open_pickle('final_vocabulary')
+vocabulary = data_frame_creator.open_pickle('final_vocabulary.pickle')
 
 inverse_network_genes = data_frame_creator.open_pickle('inverse_network_genes.pickle')
 inverse_network_drugs = data_frame_creator.open_pickle('inverse_network_drugs.pickle')
