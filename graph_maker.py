d29cac7daf481daf19606f85d5d3164657c13d57
ale <alewolf@gmail.com>
2017-09-19 10:25:02 -0700

    fixes

--- graph_maker.py
+++ graph_maker.py
@@ -55,7 +55,7 @@ def process_genes(genes):
         processed_gene_list=list(network_genes.values())
         #processed_gene_list = ['a']
     else:
-        gene_list=genes.strip(' ').split(',')
+        gene_list=genes.replace(' ','').split(',')
         print(gene_list)
         processed_gene_list = []
         for id in gene_list:
